ed924183544ca29e059138c5e07a68eb3a8727eb
Huriosity <vetani3123@mail.ru>
2020-10-05 21:49:04 +0300

    feat: complete human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
+const { parse } = require("semver");
+
 module.exports = function toReadable (number) {
-  
+    number = number.toString();
+    if (number.length === 3) {
+        let dozens = configureDozens(number.slice(1));
+        if (dozens !== "") {
+            return configureDigits(number[0]) + " hundred" + " " + dozens; 
+        }
+        return configureDigits(number[0]) + " hundred"; 
+    } else if (number.length === 2) {
+        return configureDozens(number);
+    } else {
+        return configureDigits(number[0]);
+    }
+
+}
+
+function configureDozens(number) {
+    let tens = ["", "ten","twenty", "thirty", "forty", "fifty", "sixty" , "seventy", "eighty", "ninety"];
+    switch(number) {
+        case "10": {
+            return "ten";
+        }
+        case "11": {
+            return "eleven";
+        }
+        case "12": {
+            return "twelve";
+        }
+        case "14": {
+            return "fourteen";
+        }
+        default: {
+            if (number[1] == "0") {
+                return tens[number[0]];
+            } else if (parseInt(number) >= 12 && parseInt(number) <= 19){
+                return tens[parseInt(number[1])].replace("ty","teen");
+            }
+            let result = tens[number[0]];
+            if (result === "") {
+                return configureDigits(number[1]);
+            }
+            return result + " " + configureDigits(number[1]);
+        }
+    }
+}
+
+function configureDigits(digit) {
+    let digits = ["zero","one", "two", "three", "four" , "five", "six","seven", "eight", "nine"];
+    return digits[digit];
 }
